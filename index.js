91d0e8078bf8d466f1468bd1edbdde3b0c8032ae
Karen Polanco <polanco.k@gmail.com>
2022-11-01 20:15:15 -0400

    commit tree 07e5a2fea724fbf7497818fbb02aa81bd2060fbb

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance){
+  console.log(distance)
+  if(distance < 400) {
+  return 'This one is on me!'} 
+  if(distance > 400 && distance < 2000) {
+  return 'That will be twenty bucks.'}
+  else if(distance <= 2500) {
+  return 'I will gladly take your thirty bucks.'}
+  else if (distance > 2500) {
+  return 'No can do.'}
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(location){
+  if (location === 'NYC') {
+    return 'Ok, sounds good.'
+  }
+  else if (location === 'Pittsburgh') {
+    return 'No go.'
+  }
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(money){
+  switch(money){
+  case "generous": 
+  return 'Thank you so much.';
+  break;
+  case "not as generous":
+  return 'Thank you.'
+  break;
+  default: 'thanks for everything'
+  return 'Bye.'
+}
 }
